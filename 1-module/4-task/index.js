750d367c4284c070bfed7e7a931ed904b7215383
BagyBug <BagyBug@users.noreply.github.com>
2020-08-13 19:01:02 +0300

    finish 1-4

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,13 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+  let upperStr = str.toUpperCase();
+
+  if (upperStr == '1XbeT now'.toUpperCase() || upperStr == 'free xxxxx'.toUpperCase()) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
+
