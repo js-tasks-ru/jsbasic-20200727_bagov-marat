5ea5b5f2b8aee05986451ea7e2bc0627fe18176c
BagyBug <BagyBug@users.noreply.github.com>
2020-08-19 18:13:18 +0300

    Update index.js

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,16 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let prop in salaries) {
+    let typeProp = typeof(salaries[prop]);
+    if (typeProp == 'number') {
+      sum += salaries[prop];
+
+    } else {
+      sum == 0;
+    }
+
+  }
+  return sum;
 }
