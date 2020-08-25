3fbc692bfff528b7ff95fc20a8b4ccded5b1a616
BagyBug <BagyBug@users.noreply.github.com>
2020-08-25 22:02:17 +0300

    finished module 2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,20 @@
 let calculator = {
-  // ваш код
+  
 };
 
+
+calculator.read = function (a,b) {
+    this.a = a;
+    this.b = b;
+  }
+  
+  calculator.sum = function (a,b) {
+    return this.a + this.b;
+  }
+  
+  calculator.mul = function (a,b) {
+    return this.a * this.b;
+  }
+  
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
